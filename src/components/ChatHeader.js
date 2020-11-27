9bdd9e33b373d48e73d066a231e3760c87517102
Shrihari-Prakash <shrihariprakash@outlook.com>
2020-11-27 21:53:59 +0530

    - WIP: Long press option on tab names.

--- src/components/ChatHeader.js
+++ src/components/ChatHeader.js
@@ -8,6 +8,8 @@ import IconButton from "@material-ui/core/IconButton";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Fade from "@material-ui/core/Fade";
+import useLongPress from "../helpers/useLongPress";
+import TabActions from "./TabActions";
 
 const useStyles = makeStyles((theme) => ({
   avatarSmall: {
@@ -33,7 +35,13 @@ function ChatHeader({
 
   const history = useHistory();
 
+  const backspaceLongPress = useLongPress(() => {
+    console.log("long pressed");
+    setShowTabActions(true);
+  }, 500);
+
   const [currentTab, setCurrentTab] = React.useState(0);
+  const [showTabActions, setShowTabActions] = React.useState(0);
 
   const user = useSelector((state) => {
     return state.userReducer.user;
@@ -121,6 +129,7 @@ function ChatHeader({
                         changeRenderedTab(tab);
                         setCurrentTab(index);
                       }}
+                      {...backspaceLongPress}
                     >
                       <span style={{ verticalAlign: "baseline" }}>
                         {tab.tab_name}
@@ -141,6 +150,10 @@ function ChatHeader({
                     className={classes.iconButton}
                   ></AddIcon>
                 </IconButton>
+                <TabActions
+                  showTabActions={showTabActions}
+                  setShowTabActions={setShowTabActions}
+                ></TabActions>
               </div>
             </Fade>
           </>
---
+++ src/components/TabActions.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+export default function TabActions({showTabActions, setShowTabActions}) {
+
+  const handleClose = () => {
+    setShowTabActions(false);
+  };
+
+  return (
+    <div>
+      <Dialog
+        open={showTabActions}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Confirm delete"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Do You want to delete this tab? This action is irreversible.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            No
+          </Button>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
