e06ba8eee7fbd64e67c88b31e22db1d0000a72d5
Shrihari-Prakash <shrihariprakash@outlook.com>
2021-01-15 23:24:38 +0530

    - Removed unused import.

--- src/pages/NewConversation.js
+++ src/pages/NewConversation.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import TextField from "@material-ui/core/TextField";
 import FormHelperText from "@material-ui/core/FormHelperText";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
